a935bb6d94341956faec8beaef7937914f45a044
Daniel Anderson <dan.w.anderson@gmail.com>
2020-11-05 14:28:48 -0500

    updated to add compression option, prune old files

--- backup_plex.sh
+++ backup_plex.sh
@@ -11,21 +11,23 @@ BACKUP_FILE="${HOME}/plex_backup_${NOW}.tar"
 LOCK_FILE="${HOME}/backup_plex_lock"
 # NFS mount
 BACKUP_DIR="/mnt/Plex/backup"
+REMOVE_OLD="180"
+COMPRESS=""
 
 function finish {
-    rm -f "$LOCK_FILE"
+    rm -f "${LOCK_FILE}"
 }
 trap finish EXIT SIGINT SIGTERM
 
-if [ -f "$LOCK_FILE" ];
+if [ -f "${LOCK_FILE}" ];
 then
     echo "Lock file exists. Is there another backup running?"
     exit 2
 else
-    touch "$LOCK_FILE"
+    touch "${LOCK_FILE}"
 fi
 
-if [ ! -d "$PLEX_LIBRARY_DIR" ];
+if [ ! -d "${PLEX_LIBRARY_DIR}" ];
 then
     echo "Plex library directory not found"
     exit 1
@@ -44,11 +46,22 @@ then
     exit 1
 fi
 
-cd "$PLEX_LIBRARY_DIR" || exit
+if [ "${REMOVE_OLD}" ];
+then
+    find "${BACKUP_DIR}" -mtime +"${REMOVE_OLD}" -type f -iname "plex_backup*.tar" -exec /bin/rm -v {} \;
+fi
 
-tar --exclude="Cache/*" --exclude="Crash Reports/*" -cvf "$BACKUP_FILE" ./*
+cd "${PLEX_LIBRARY_DIR}" || exit
 
-mv "$BACKUP_FILE" "$BACKUP_DIR"
+tar --exclude="Cache/*" --exclude="Crash Reports/*" -cvf "${BACKUP_FILE}" ./*
+
+if ! [ -z "${COMPRESS}" ];
+then
+    gzip "${BACKUP_FILE}"
+    BACKUP_FILE="${BACKUP_FILE}.gz"
+fi
+
+mv "${BACKUP_FILE}" "${BACKUP_DIR}"
 
 BACKUP_FILENAME=$(basename "${BACKUP_FILE}")
 
